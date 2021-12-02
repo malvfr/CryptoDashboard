65c6a4f9ab83d77fd5e9cbf83f05eb52d22d0462
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2021-12-02 20:47:24 -0300

    Add credo

--- mix.exs
+++ mix.exs
@@ -50,7 +50,9 @@ defmodule CryptoDashboard.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:ueberauth, "~> 0.6"},
-      {:ueberauth_github, "~> 0.7"}
+      {:ueberauth_github, "~> 0.7"},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
